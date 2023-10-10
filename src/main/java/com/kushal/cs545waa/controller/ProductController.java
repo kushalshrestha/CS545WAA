cffd12771dd6bb71e07b0a7925c2e02706b81b57
kushalshrestha <kushalshr@gmail.com>
2023-10-10 06:49:18 -0500

    fix : security scan

--- src/main/java/com/kushal/cs545waa/controller/ProductController.java
+++ src/main/java/com/kushal/cs545waa/controller/ProductController.java
@@ -2,11 +2,15 @@ package com.kushal.cs545waa.controller;
 
 
 import com.kushal.cs545waa.model.Product;
-import org.springframework.http.ResponseEntity;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.net.URLEncoder;
 import java.util.Arrays;
 
 @Controller
@@ -29,7 +33,10 @@ public class ProductController {
     }
 
     @GetMapping("/getProduct")
-    public String getProductsById(@RequestParam(name = "productId") int id, Model model) {
+    public String getProductsById(
+            @RequestParam(name = "productId") int id,
+            Model model
+    ) {
         Product p = Product.builder()
                            .id(id)
                            .title("Title " + id)
@@ -40,11 +47,49 @@ public class ProductController {
         return "product";
     }
 
-    @GetMapping("/delete/{id}")
-    public String deleteProduct(@PathVariable("id") int productId, Model model) {
-        String message = "Product id : " + productId + " deleted successfully!!!";
-        model.addAttribute("response", message);
-        return "product-delete";
+
+    @GetMapping("/greet")
+    public String greet(@RequestParam("name") String name, Model model) {
+        Product p1 = new Product(1, "Title 1", "Description 1", 900);
+        Product p2 = Product.builder()
+                            .id(2)
+                            .title("Title 2")
+                            .description("Description 2")
+                            .price(100)
+                            .build();
+        var products = Arrays.asList(p1, p2);
+        model.addAttribute("products", products);
+        return "products";
     }
+
+
+
+
+
+
+
+
+//    @GetMapping("/delete/{id}")
+//    public String deleteProduct(@PathVariable("id") int productId, Model model) {
+//        String message = "Product id : " + productId + " deleted successfully!!!";
+//        model.addAttribute("response", message);
+//        return "product-delete";
+//    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteProduct(
+            @PathVariable("id") String productId,
+            Model model, HttpServletRequest request
+    ) {
+        try {
+            int parameterIntValue = Integer.parseInt(productId);
+            String message = "Product id : " + parameterIntValue + " deleted successfully!!!";
+            model.addAttribute("response", message);
+            return "product-delete";
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("The parameter value is not a valid integer");
+        }
+    }
+
 
 }
